82a00f381d67b619f46b86ade1a6ec205f161ae3
DeLucasso <dumdumcz@gmail.com>
2022-06-15 21:19:04 -0700

    Initial Init + added Mongoose, added mongoose Model + Schema + Document

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
+const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,53 +15,62 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
-app.get("/", function(req, res) {
-    res.render("home", {
+let posts = [];
+let id = 1;
+let todaysDate = {};
+app.get("/", function(req, res){
+  res.render("home", {
     startingContent: homeStartingContent,
     posts: posts
-  });
+    });
 });
 
-app.get("/about", function(req, res) {
-  res.render("about", {
-    aboutContents: aboutContent
-  });
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
 });
 
-app.get("/contact", function(req, res) {
-  res.render("contact", {
-    contactContents: contactContent
-  });
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
-app.get("/compose", function(req, res) {
+app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-let id = 0;
-
-app.post("/compose", function(req, res) {
-  let postTitle = req.body.postTitle;
-  let postBody = req.body.postBody;
-  id++;
-
+app.post("/compose", function(req, res){
+  let day = date.getDate();
   const post = {
-    id: id,
+    id : id,
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
+    dateCreated: day
   };
-
+  id++;
   posts.push(post);
-  // this is a redirect to get request route "/" back to line 20
+
   res.redirect("/");
+
 });
 
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+        id: id
+      });
+    }
+  });
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
